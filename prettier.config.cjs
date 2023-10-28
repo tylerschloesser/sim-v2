6627887e80013aa23bc5f9e6d332017b0858e263
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-27 17:59:51 -0700

    add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  importOrder: ['^[./]'],
+  printWidth: 60,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+}
