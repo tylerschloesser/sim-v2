236f47d10e6080574cafe1456958245d1a4c660f
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-29 16:57:56 -0700

    show strategy

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,9 +1,14 @@
 import { SimulatorStrategy } from '@sim-v2/types'
+import invariant from 'tiny-invariant'
 import { initApp } from './app.js'
 import './index.scss'
 
+const debug = document.getElementById('debug')
+invariant(debug)
+
 let strategy: SimulatorStrategy = SimulatorStrategy.Local
 let app = await initApp(strategy)
+debug.innerText = `strategy: ${strategy}`
 
 window.addEventListener('keyup', async (e) => {
   if (e.key === '.') {
@@ -14,5 +19,6 @@ window.addEventListener('keyup', async (e) => {
       strategy = SimulatorStrategy.Local
     }
     app = await initApp(strategy)
+    debug.innerText = `strategy: ${strategy}`
   }
 })
