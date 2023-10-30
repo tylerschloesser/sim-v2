2056f8ca3c73f29e69ccc94e1d5ddae4d662fc64
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-29 17:08:40 -0700

    add toggle button

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -3,22 +3,34 @@ import invariant from 'tiny-invariant'
 import { initApp } from './app.js'
 import './index.scss'
 
-const debug = document.getElementById('debug')
+const debug = document.querySelector<HTMLDivElement>(
+  '.debug__strategy',
+)!
 invariant(debug)
+const toggle = document.querySelector<HTMLButtonElement>(
+  '.debug__toggle',
+)!
+invariant(toggle)
 
 let strategy: SimulatorStrategy = SimulatorStrategy.Local
 let app = await initApp(strategy)
 debug.innerText = `strategy: ${strategy}`
 
+async function toggleStrategy() {
+  app.destroy()
+  if (strategy === SimulatorStrategy.Local) {
+    strategy = SimulatorStrategy.WebWorker
+  } else {
+    strategy = SimulatorStrategy.Local
+  }
+  app = await initApp(strategy)
+  debug.innerText = `strategy: ${strategy}`
+}
+
 window.addEventListener('keyup', async (e) => {
   if (e.key === '.') {
-    app.destroy()
-    if (strategy === SimulatorStrategy.Local) {
-      strategy = SimulatorStrategy.WebWorker
-    } else {
-      strategy = SimulatorStrategy.Local
-    }
-    app = await initApp(strategy)
-    debug.innerText = `strategy: ${strategy}`
+    toggleStrategy()
   }
 })
+
+toggle.addEventListener('pointerup', toggleStrategy)
