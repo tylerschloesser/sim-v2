55b87459eab9437c486aaa8b945e47d55857e3ad
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-30 11:48:56 -0700

    add load shader

--- packages/graphics/src/init-gpu-graphics.ts
+++ packages/graphics/src/init-gpu-graphics.ts
@@ -1,7 +1,11 @@
 import { Graphics, Viewport } from '@sim-v2/types'
 import invariant from 'tiny-invariant'
+import frag from './frag.glsl'
 import { getGpuContext } from './util.js'
 import vert from './vert.glsl'
+
+type ShaderType = number
+type ShaderSource = string
 
 export function initGpuGraphics({
   canvas,
@@ -13,6 +17,11 @@ export function initGpuGraphics({
   const gl = getGpuContext(canvas)
   invariant(gl)
 
+  const shaders = {
+    vert: loadShader(gl, gl.VERTEX_SHADER, vert),
+    frag: loadShader(gl, gl.FRAGMENT_SHADER, frag),
+  }
+
   gl.clearColor(0, 0, 0, 1)
   gl.clear(gl.COLOR_BUFFER_BIT)
 
@@ -22,4 +31,19 @@ export function initGpuGraphics({
     stop() {},
     move(delta) {},
   }
+}
+
+function loadShader(
+  gl: WebGL2RenderingContext,
+  type: ShaderType,
+  source: ShaderSource,
+): WebGLShader {
+  const shader = gl.createShader(type)
+  invariant(shader)
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  invariant(
+    gl.getShaderParameter(shader, gl.COMPILE_STATUS) !== 0,
+  )
+  return shader
 }
--- packages/graphics/src/vert.glsl
+++ packages/graphics/src/vert.glsl
@@ -2,5 +2,5 @@ attribute vec4 aVertexPosition;
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 void main() {
-	gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
 }
