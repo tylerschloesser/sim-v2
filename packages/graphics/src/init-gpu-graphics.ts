365e9d221775a41063a9bbb866a626c446093523
Tyler Schloesser <tylerschloesser@gmail.com>
2023-10-30 11:51:02 -0700

    init program

--- packages/graphics/src/init-gpu-graphics.ts
+++ packages/graphics/src/init-gpu-graphics.ts
@@ -7,6 +7,11 @@ import vert from './vert.glsl'
 type ShaderType = number
 type ShaderSource = string
 
+type Shaders = {
+  vert: WebGLShader
+  frag: WebGLShader
+}
+
 export function initGpuGraphics({
   canvas,
   viewport,
@@ -17,10 +22,12 @@ export function initGpuGraphics({
   const gl = getGpuContext(canvas)
   invariant(gl)
 
-  const shaders = {
+  const shaders: Shaders = {
     vert: initShader(gl, gl.VERTEX_SHADER, vert),
     frag: initShader(gl, gl.FRAGMENT_SHADER, frag),
   }
+
+  const program = initProgram(gl, shaders)
 
   gl.clearColor(0, 0, 0, 1)
   gl.clear(gl.COLOR_BUFFER_BIT)
@@ -31,6 +38,21 @@ export function initGpuGraphics({
     stop() {},
     move(delta) {},
   }
+}
+
+function initProgram(
+  gl: WebGL2RenderingContext,
+  shaders: Shaders,
+) {
+  const program = gl.createProgram()
+  invariant(program)
+  gl.attachShader(program, shaders.vert)
+  gl.attachShader(program, shaders.frag)
+  gl.linkProgram(program)
+  invariant(
+    gl.getProgramParameter(program, gl.LINK_STATUS) !== 0,
+  )
+  return program
 }
 
 function initShader(
