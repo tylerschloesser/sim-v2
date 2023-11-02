136e7f4b9f550df541c781baaca6a6cd23557d2a
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-02 12:18:12 -0700

    update random tile

--- packages/simulator/src/generate-chunk.ts
+++ packages/simulator/src/generate-chunk.ts
@@ -1,4 +1,4 @@
-import { random } from '@sim-v2/util'
+import { memo, random } from '@sim-v2/util'
 import {
   Chunk,
   ChunkId,
@@ -6,6 +6,12 @@ import {
   TileType,
 } from '@sim-v2/world'
 import invariant from 'tiny-invariant'
+
+const getRandomTile = memo((_key: string): Tile => {
+  return {
+    type: random(Object.values(TileType)),
+  }
+})
 
 export function generateChunk({
   chunkId,
@@ -19,9 +25,7 @@ export function generateChunk({
   let i = 0
   for (let y = 0; y < chunkSize; y++) {
     for (let x = 0; x < chunkSize; x++) {
-      tiles[i++] = {
-        type: random(Object.values(TileType)),
-      }
+      tiles[i++] = getRandomTile(`${chunkId}.${x}.${y}`)
     }
   }
 
