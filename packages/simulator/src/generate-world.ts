defeedc56b711c240286d1a16ccc1782b29228e6
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-03 00:10:50 -0700

    refactor initial chunks

--- packages/simulator/src/generate-world.ts
+++ packages/simulator/src/generate-world.ts
@@ -1,7 +1,10 @@
 import { World, WorldId } from '@sim-v2/world'
 import invariant from 'tiny-invariant'
 import { generateChunk } from './generate-chunk.js'
-import { initGeneratorContext } from './init-generator-context.js'
+import {
+  GeneratorContext,
+  initGeneratorContext,
+} from './init-generator-context.js'
 
 const TICK_DURATION = 100
 const CHUNK_SIZE = 32
@@ -15,31 +18,13 @@ export function generateWorld({
   seed: string
 }): World {
   const generator = initGeneratorContext(seed)
-
   const tickDuration = TICK_DURATION
   const chunkSize = CHUNK_SIZE
-
-  const chunks: World['chunks'] = {}
-
-  for (
-    let x = -INITIAL_CHUNK_RADIUS;
-    x < INITIAL_CHUNK_RADIUS;
-    x++
-  ) {
-    for (
-      let y = -INITIAL_CHUNK_RADIUS;
-      y < INITIAL_CHUNK_RADIUS;
-      y++
-    ) {
-      const chunkId = `${x}.${y}`
-      invariant(!chunks[chunkId])
-      chunks[chunkId] = generateChunk({
-        chunkId,
-        chunkSize,
-        generator,
-      })
-    }
-  }
+  const chunks = generateInitialChunks({
+    radius: INITIAL_CHUNK_RADIUS,
+    chunkSize,
+    generator,
+  })
 
   return {
     id,
@@ -49,4 +34,28 @@ export function generateWorld({
     tick: 0,
     chunks,
   }
+}
+
+function generateInitialChunks({
+  radius,
+  chunkSize,
+  generator,
+}: {
+  radius: number
+  chunkSize: number
+  generator: GeneratorContext
+}): World['chunks'] {
+  const chunks: World['chunks'] = {}
+  for (let x = -radius; x < radius; x++) {
+    for (let y = -radius; y < radius; y++) {
+      const chunkId = `${x}.${y}`
+      invariant(!chunks[chunkId])
+      chunks[chunkId] = generateChunk({
+        chunkId,
+        chunkSize,
+        generator,
+      })
+    }
+  }
+  return chunks
 }
