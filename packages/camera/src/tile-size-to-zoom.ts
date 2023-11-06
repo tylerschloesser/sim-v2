c8e83c5cb83cd3f3012dfc5250aa6806f37f37bb
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-06 12:39:59 -0800

    cache tile size

--- packages/camera/src/tile-size-to-zoom.ts
+++ packages/camera/src/tile-size-to-zoom.ts
@@ -3,13 +3,43 @@ import { clamp } from '@sim-v2/util'
 import { MAX_ZOOM, MIN_ZOOM } from './const.js'
 import { getMinMaxTileSize } from './util.js'
 
+const lastTileSizeToZoomArgs = {
+  tileSize: 0,
+  viewportSize: {
+    x: 0,
+    y: 0,
+  },
+}
+
+let lastZoom: number = 0
+
 export function tileSizeToZoom(
   tileSize: number,
   viewport: Viewport,
 ): number {
+  //
+  // cache the last zoom because the vast majority of calls
+  // to this function will happen when the camera position, but
+  // not tile size, changes.
+  //
+  if (
+    lastTileSizeToZoomArgs.tileSize === tileSize &&
+    lastTileSizeToZoomArgs.viewportSize.x ===
+      viewport.size.x &&
+    lastTileSizeToZoomArgs.viewportSize.y ===
+      viewport.size.y
+  ) {
+    return lastZoom
+  }
+
   const { minTileSize, maxTileSize } =
     getMinMaxTileSize(viewport)
   const zoom =
     (tileSize - minTileSize) / (maxTileSize - minTileSize)
-  return clamp(zoom, MIN_ZOOM, MAX_ZOOM)
+
+  lastTileSizeToZoomArgs.tileSize = tileSize
+  lastTileSizeToZoomArgs.viewportSize.x = viewport.size.x
+  lastTileSizeToZoomArgs.viewportSize.y = viewport.size.y
+
+  return (lastZoom = clamp(zoom, MIN_ZOOM, MAX_ZOOM))
 }
