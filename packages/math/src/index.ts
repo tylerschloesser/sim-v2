c7248e9d61bc78d8ad192cad54b8111c5518c6df
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-07 16:36:29 -0800

    cleanup

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -10,10 +10,4 @@ export const easeIn: TimingFn = (k) => {
   return k ** 2
 }
 
-export const easeInOut: TimingFn = (k: number) => {
-  return 1 - 0.5 * (Math.sin((k - 0.5) * Math.PI) + 1)
-}
-
-console.log(easeInOut(0.1))
-
 export const linear: TimingFn = (k) => k
